ecd7044d02f858192e954cb3973c0e55b157421c
Marlene Orth <marleneorth@Marlenes-MBP.fritz.box>
2019-06-16 21:58:05 +0200

    use bcrypt hashing

--- app.js
+++ app.js
@@ -4,11 +4,16 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+// mongoose-encryption removed because we have switched to MD5
+// const encrypt = require("mongoose-encryption");
+// const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10; // for 2019 and 2020
+
 
 const app = express();
 
-console.log(process.env.API_KEY);
+// console.log(process.env.API_KEY);
 
 // express: this line serves images, CSS files and JavaScript files in a directory named 'public'
 app.use(express.static("public"));
@@ -22,12 +27,14 @@ app.use(bodyParser.urlencoded({
 // specify the URL where our mongoDB database is located
 // 27017 is the default port
 // after the forward slash comes the name of our database, in this case userDB
-mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/userDB", {
+  useNewUrlParser: true
+});
 
 // ** set up a new user database **
 
 // Step 1: Create a new schema so we can use mongoose-encryption
-const userSchema = new mongoose.Schema ({
+const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
@@ -37,9 +44,10 @@ const userSchema = new mongoose.Schema ({
 // use that secret string to encrypt your database
 // to the schema create above (l.28) add the encryption plug in and pass over our secret
 //   string as a JavaScript object
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
+// userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 // Important: the encryption plugin has to be added to the schema before the model
 //   is created because we pass in the userSchema as a parameter to create the User model
+// *** PLUG IN FOR USER SCHEMA WAS REMOVED BECAUSE WE MOVED TO MD5 HASHING ***
 
 
 // Step 2: Use the schema to set up a new User model
@@ -50,45 +58,59 @@ const User = new mongoose.model("User", userSchema);
 
 
 // render the homepage
-app.get("/", function(req, res){
+app.get("/", function(req, res) {
   res.render("home");
 });
 
 // render the login route
-app.get("/login", function(req, res){
+app.get("/login", function(req, res) {
   res.render("login");
 });
 
 // render the register route
-app.get("/register", function(req, res){
+app.get("/register", function(req, res) {
   res.render("register");
 });
 
 // catch post request from the form the user submits on the register homepage
 
-app.post("/register", function(req,res){
-  // inside the callback function we create the user using the information that they passed over
-  //   from the register page
-  // in order to grab the data from the body of the post request we tap into the name variables
-  //   of the username and password inputs
-  const newUser = new User({
-    email: req.body.username,
-    password: req.body.password
+app.post("/register", function(req, res) {
+
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    // inside the callback function we now have access to the hash that we can store in our DB
+    //   and use as our password (see line 88x)
+
+    // inside the callback function we create the user using the information that they passed over
+    //   from the register page
+    // in order to grab the data from the body of the post request we tap into the name variables
+    //   of the username and password inputs
+    const newUser = new User({
+      email: req.body.username,
+      // as the password we use the hash that we generated before
+      password: hash
+    });
+
+    // save that new user with their email and their hashed password into our database
+    newUser.save(function(err) {
+      if (err) {
+        console.log(err)
+      } else {
+        res.render("secrets");
+      }
+    });
   });
 
-  // save that new user
-  newUser.save(function(err){
-    if(err) {
-      console.log(err)
-    } else {
-      res.render("secrets");
-    }
-  });
-  });
+});
+
+
+
+
+
+
 
 // catch the post request from the login route
 
-app.post("/login", function(req,res){
+app.post("/login", function(req, res) {
   // here we check whether we actually have a user in our database with the credentials they put in
   // the credentials we are checking is the username and the password
   const username = req.body.username;
@@ -96,16 +118,22 @@ app.post("/login", function(req,res){
 
   // check those constants against our database, i.e. look through our database and see if the name
   // the user typed in is equal to one in the database
-  User.findOne({email: username}, function(err, foundUser){
-    if(err) {
+  User.findOne({
+    email: username
+  }, function(err, foundUser) {
+    if (err) {
       console.log(err);
     } else {
       // if there is a user on our database with our email, we check whether the password in the
       //  database corresponds to the one the user typed in
       if (foundUser) {
-        if (foundUser.password === password) {
-          res.render("secrets");
-        }
+
+        // Load hash from your password DB.
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+          if(result === true){
+            res.render("secrets");
+          }
+        });
       }
     }
   });
