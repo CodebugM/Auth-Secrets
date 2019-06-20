7696744da443768d154f7d72922c3906d51b5daf
Marlene Orth <marleneorth@Marlenes-MBP.fritz.box>
2019-06-20 12:46:41 +0200

    Add passport to app.js

--- app.js
+++ app.js
@@ -4,11 +4,18 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-// mongoose-encryption removed because we have switched to MD5
+// * * mongoose-encryption, MD5 and bcrypt constants - removed because we have switched to passport * * //
 // const encrypt = require("mongoose-encryption");
 // const md5 = require("md5");
-const bcrypt = require("bcrypt");
-const saltRounds = 10; // for 2019 and 2020
+// const bcrypt = require("bcrypt");
+// const saltRounds = 10; // 10 are sufficient for 2019 and 2020
+
+// * * passport packages * * ORDER IMPORTANT!
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+// note: the passport-local package will be use by passport-local-mongoose, so we don't need
+//   to require it specifically here
 
 
 const app = express();
@@ -23,6 +30,20 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+// ! important to specify the app.use() code for express-session BEFORE mongoose.connect() and
+//   AFTER the app.use() code of express
+// app.use(session()) sets up the session with some initial configurations
+app.use(session({
+  secret: "XXX",
+  resave: false,
+  saveUninitialized: true,
+}));
+
+// initialise passport - sets up passport so we can use it for authentication
+app.use(passport.initialize()); // tells our app to use passport and to initialise it
+app.use(passport.session()); // tells our app to use passport to deal with the session
+
+
 // * use mongoose to connect to our mongoDB *
 // specify the URL where our mongoDB database is located
 // 27017 is the default port
@@ -30,6 +51,8 @@ app.use(bodyParser.urlencoded({
 mongoose.connect("mongodb://localhost:27017/userDB", {
   useNewUrlParser: true
 });
+// add the mongoose.set() code to get rid of the deprecation warning regarding index use
+mongoose.set("useCreateIndex", true);
 
 // ** set up a new user database **
 
@@ -38,6 +61,9 @@ const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
+
+userSchema.plugin(passportLocalMongoose);
+
 
 // instead of using an encryption and a sign-in key, one can use a secret string
 // *** HAS NOW MOVED TO THE .ENV FILE ***
@@ -54,6 +80,11 @@ const userSchema = new mongoose.Schema({
 // as always, we speciy the name of our collection in the singular form: users --> User
 const User = new mongoose.model("User", userSchema);
 
+// passport local configurations
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 
 
@@ -72,33 +103,61 @@ app.get("/register", function(req, res) {
   res.render("register");
 });
 
+app.get("/secrets", function(req, res){
+  // if a user is already logged in, we want to simply render the secrets page
+  // if not, we redirect them to the secrets page
+  if(req.isAuthenticated()){
+    res.render("secrets");
+  } else {
+    res.redirect("/login");
+  }
+});
+
+
 // catch post request from the form the user submits on the register homepage
 
 app.post("/register", function(req, res) {
 
-  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-    // inside the callback function we now have access to the hash that we can store in our DB
-    //   and use as our password (see line 88x)
-
-    // inside the callback function we create the user using the information that they passed over
-    //   from the register page
-    // in order to grab the data from the body of the post request we tap into the name variables
-    //   of the username and password inputs
-    const newUser = new User({
-      email: req.body.username,
-      // as the password we use the hash that we generated before
-      password: hash
-    });
-
-    // save that new user with their email and their hashed password into our database
-    newUser.save(function(err) {
-      if (err) {
-        console.log(err)
-      } else {
-        res.render("secrets");
-      }
-    });
+  // tap into the User model and call the register method on it
+  // this method comes from the passport-local-mongoose package
+  User.register({username: req.body.username}, req.body.password, function(err, user){
+    if(err) {
+      console.log(err);
+      res.redirect("/register");
+    } else {
+      passport.authenticate("local")(req, res, function(){
+        res.redirect("/secrets");
+      });
+    }
   });
+
+
+
+  // * * * OLD CODE USING BCRYPT * * *
+
+  // bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+  //   // inside the callback function we now have access to the hash that we can store in our DB
+  //   //   and use as our password (see line 88x)
+  //
+  //   // inside the callback function we create the user using the information that they passed over
+  //   //   from the register page
+  //   // in order to grab the data from the body of the post request we tap into the name variables
+  //   //   of the username and password inputs
+  //   const newUser = new User({
+  //     email: req.body.username,
+  //     // as the password we use the hash that we generated before
+  //     password: hash
+  //   });
+  //
+  //   // save that new user with their email and their hashed password into our database
+  //   newUser.save(function(err) {
+  //     if (err) {
+  //       console.log(err)
+  //     } else {
+  //       res.render("secrets");
+  //     }
+  //   });
+  // });
 
 });
 
@@ -111,32 +170,42 @@ app.post("/register", function(req, res) {
 // catch the post request from the login route
 
 app.post("/login", function(req, res) {
-  // here we check whether we actually have a user in our database with the credentials they put in
-  // the credentials we are checking is the username and the password
-  const username = req.body.username;
-  const password = req.body.password;
 
-  // check those constants against our database, i.e. look through our database and see if the name
-  // the user typed in is equal to one in the database
-  User.findOne({
-    email: username
-  }, function(err, foundUser) {
-    if (err) {
-      console.log(err);
-    } else {
-      // if there is a user on our database with our email, we check whether the password in the
-      //  database corresponds to the one the user typed in
-      if (foundUser) {
 
-        // Load hash from your password DB.
-        bcrypt.compare(password, foundUser.password, function(err, result) {
-          if(result === true){
-            res.render("secrets");
-          }
-        });
-      }
-    }
-  });
+
+
+
+
+
+
+// * * * OLD CODE USING BCRYPT * * * //
+
+  // // here we check whether we actually have a user in our database with the credentials they put in
+  // // the credentials we are checking is the username and the password
+  // const username = req.body.username;
+  // const password = req.body.password;
+  //
+  // // check those constants against our database, i.e. look through our database and see if the name
+  // // the user typed in is equal to one in the database
+  // User.findOne({
+  //   email: username
+  // }, function(err, foundUser) {
+  //   if (err) {
+  //     console.log(err);
+  //   } else {
+  //     // if there is a user on our database with our email, we check whether the password in the
+  //     //  database corresponds to the one the user typed in
+  //     if (foundUser) {
+  //
+  //       // Load hash from your password DB.
+  //       bcrypt.compare(password, foundUser.password, function(err, result) {
+  //         if(result === true){
+  //           res.render("secrets");
+  //         }
+  //       });
+  //     }
+  //   }
+  // });
 });
 
 
