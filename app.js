de1a66fd9129dbb228e01b414079109c1fba6501
Marlene Orth <marlene.orth@gmail.com>
2019-07-15 18:28:31 +0200

    Add Facebook strategy to authenticate users

--- app.js
+++ app.js
@@ -124,6 +124,19 @@ passport.use(new GoogleStrategy({
   }
 ));
 
+// code for the passport facebook authentication package
+passport.use(new FacebookStrategy({
+    clientID: process.env.FACEBOOK_APP_ID,
+    clientSecret: process.env.FACEBOOK_APP_SECRET,
+    callbackURL: "http://localhost:3000/auth/facebook/secrets"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
 
 
 // render the homepage
